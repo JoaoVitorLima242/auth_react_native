af1e902702f64f96d1e468e1e96fe23cc9a8e34e
JoaoVitorLima242 <jlima0500@gmail.com>
2022-11-02 20:08:55 -0300

    Getting messages

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,10 +1,22 @@
+import axios from 'axios';
+import { useEffect, useState, useContext} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { AuthContext } from '../contexts/auth';
+
 
 function WelcomeScreen() {
+  const [message, setMessage] = useState('')
+  const {token} = useContext(AuthContext) 
+  useEffect(() => {
+    axios.get(`https://react-native-637a1-default-rtdb.firebaseio.com/message.json?auth=${token}`)
+    .then((response) => setMessage(response.data))
+  }, [])
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{message}</Text>
     </View>
   );
 }
